87170d513842c720676f7e322d26278832334b28
Vignesh Ezhil <86967110+viggy2000@users.noreply.github.com>
2022-04-01 23:51:06 +0800

    completed subTotal

--- hotel/src/main/java/com/hotel/controller/PaymentController.java
+++ hotel/src/main/java/com/hotel/controller/PaymentController.java
@@ -2,18 +2,31 @@ package com.hotel.controller;
 
 import com.hotel.db.PaymentDB;
 import com.hotel.system.Payment;
+import com.hotel.system.Reservation;
 import com.hotel.system.Order;
 import com.hotel.system.Item;
+import com.hotel.system.Room;
+import com.hotel.controller.RoomController;
+import com.hotel.controller.OrderController;
+
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Date;
+import java.time.Period;
 
 public class PaymentController {
 
     static PaymentDB allPayments= new PaymentDB();
 
-    static private double serviceCharge;
-    static private double gst;
+    static OrderController orderController = new OrderController();
+
+    static private double serviceCharge = 0.10;
+    static private double gst = 0.07;
+
+
+   
 
     public static ArrayList getAllPayments() throws IOException {
         ArrayList allData = allPayments.read(allPayments.getPath());
@@ -29,6 +42,49 @@ public class PaymentController {
             System.out.println("Order not added!");
             System.out.println(e);
         }
+    }
+
+    public static double getOrderTotal(String roomId) throws IOException{
+
+        //assumption here is that the room exists
+
+        double total = 0.00;
+        
+        ArrayList<Order> orderList = orderController.getOrderListByRoomId(roomId);
+        for(Order order: orderList){
+
+            ArrayList<Item> itemList = order.getItem();
+            for(Item item:itemList){
+                total += item.getPrice();
+            }
+
+        }
+
+        return total;
+
+    }
+
+    public static double getRoomTotal(String roomId, String guestId) throws IOException{
+
+        double total =0.00;
+        Room room = RoomController.getSpecificRoom(roomId);
+        double roomRate = room.getRoomRate();
+
+        // get checkin and check out date
+
+        Reservation r = ReservationController.getReservationByRoomId(roomId);
+        Date checkIn = r.getCheckInDate();
+        Date checkOut = r.getCheckOutDate();
+
+
+        long diff = checkOut.getTime() - checkIn.getTime();
+
+        long difference_In_Days = (diff/ (1000 * 60 * 60 * 24))% 365;
+
+        total  = roomRate * Double.valueOf(difference_In_Days);
+
+        return total;
+
     }
 
     public static void main(String[] args) throws IOException {
