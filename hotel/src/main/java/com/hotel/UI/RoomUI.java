9d6c825c3d669771c584b324a5d3b021067bbfe4
Vignesh Ezhil <86967110+viggy2000@users.noreply.github.com>
2022-04-04 23:04:10 +0800

    completed UI , need to check on the roomID part

--- hotel/src/main/java/com/hotel/UI/RoomUI.java
+++ hotel/src/main/java/com/hotel/UI/RoomUI.java
@@ -154,6 +154,263 @@ public class RoomUI {
         RoomController.saveSpecificRoomByRoomId(newRoom);
     }
 
+    public static RoomType getRoomType() {
+        RoomType roomType=null;
+        do {
+            System.out.println("(1) Single\n"+
+                                "(2) Double\n"+
+                                "(3) Deluxe\n"+
+                                "(4) VIP SUITE");
+            System.out.println("Enter the RoomType of the room to add:  ");
+            int choice=0;
+            try {
+                    choice = sc.nextInt();
+
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Wrong input!");
+            }
+            if (choice >0 && choice < 5) {
+                switch (choice) {
+                    case 1:
+                        roomType = RoomType.SINGLE;
+                        break;
+                    case 2:
+                        roomType = RoomType.DOUBLE;
+                        break;
+                    case 3:
+                        roomType = RoomType.DELUXE;
+                        break;
+                    case 4:
+                        roomType = RoomType.VIP_SUITE;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else System.out.println("wrong choice");
+
+
+        } while (roomType == null);
+
+        return roomType;
+    }
+
+
+    public static RoomStatus getRoomStatus() {
+        RoomStatus roomStatus=null;
+        do {
+            System.out.println("(1) Occupied\n"+
+                                "(2) Reserved\n"+
+                                "(3) Under Maintance\n"+
+                                "(4) Vacant");
+            System.out.println("Enter the RoomStatus of the room to add:  ");
+            int choice=0;
+            try {
+                    choice = sc.nextInt();
+
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Wrong input!");
+            }
+            if (choice >0 && choice < 5) {
+                switch (choice) {
+                    case 1:
+                        roomStatus = RoomStatus.OCCUPIED;
+                        break;
+                    case 2:
+                        roomStatus = RoomStatus.RESERVED;
+                        break;
+                    case 3:
+                        roomStatus = RoomStatus.UNDER_MAINTAINANCE;
+                        break;
+                    case 4:
+                        roomStatus = RoomStatus.VACANT;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else System.out.println("wrong choice");
+
+
+        } while (roomStatus == null);
+
+        return roomStatus;
+    }
+
+
+    public static BedType getBedType() {
+        BedType bedType=null;
+        do {
+            System.out.println("(1) Single\n"+
+                                "(2) Super - Single\n"+
+                                "(3) Double\n"+
+                                "(4) Queen\n"+
+                                "(5) King");
+            System.out.println("Enter the BedType of the room to add:  ");
+            int choice=0;
+            try {
+                    choice = sc.nextInt();
+
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Wrong input!");
+            }
+            if (choice >0 && choice < 6) {
+                switch (choice) {
+                    case 1:
+                        bedType = BedType.SINGLE;
+                        break;
+                    case 2:
+                        bedType = BedType.SUPER_SINGLE;
+                        break;
+                    case 3:
+                        bedType = BedType.DOUBLE;
+                        break;
+                    case 4:
+                        bedType = BedType.QUEEN;
+                        break;
+                    case 5:
+                        bedType = BedType.KING;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else System.out.println("wrong choice");
+
+
+        } while (bedType == null);
+
+        return bedType;
+    }
+
+    public static boolean getWithView() {
+        Boolean withView = null;
+        do {
+            System.out.println("(1) Yes\n"+
+                                "(2) No");
+            System.out.println("Does this room have a view:  ");
+            int choice=0;
+            try {
+                    choice = sc.nextInt();
+
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Wrong input!");
+            }
+            if (choice >0 && choice < 3) {
+                switch (choice) {
+                    case 1:
+                        withView = true;
+                        break;
+                    case 2:
+                        withView = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else System.out.println("wrong choice");
+
+
+        } while (withView == null);
+
+        return withView;
+    }
+
+    public static boolean getWifiEnabled() {
+        Boolean wifiEnabled = null;
+        do {
+            System.out.println("(1) Yes\n"+
+                                "(2) No");
+            System.out.println("Does this room have a wifi:  ");
+            int choice=0;
+            try {
+                    choice = sc.nextInt();
+
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Wrong input!");
+            }
+            if (choice >0 && choice < 3) {
+                switch (choice) {
+                    case 1:
+                        wifiEnabled = true;
+                        break;
+                    case 2:
+                        wifiEnabled = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else System.out.println("wrong choice");
+
+
+        } while (wifiEnabled == null);
+
+        return wifiEnabled;
+    }
+
+
+    public static boolean getSmokingStatus() {
+        Boolean smokingStatus = null;
+        do {
+            System.out.println("(1) Yes\n"+
+                                "(2) No");
+            System.out.println("Does this room have allow smoking:  ");
+            int choice=0;
+            try {
+                    choice = sc.nextInt();
+
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Wrong input!");
+            }
+            if (choice >0 && choice < 3) {
+                switch (choice) {
+                    case 1:
+                        smokingStatus = true;
+                        break;
+                    case 2:
+                        smokingStatus = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else System.out.println("wrong choice");
+
+
+        } while (smokingStatus == null);
+
+        return smokingStatus;
+    }
+
+    public static float getRoomRate() {
+        float price=0.0f;
+        do {
+            System.out.println("Enter the price of the Room (e.g 26.5):  ");
+            try {
+                price = sc.nextFloat();
+                if (price != 0.0) break;
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println("Wrong price input!");
+            }
+        } while (true);
+        return price;
+    }
+
+
+
+
+
+
+    
+
 
 
 
