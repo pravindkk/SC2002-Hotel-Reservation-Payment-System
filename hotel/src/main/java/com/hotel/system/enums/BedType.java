c09a06ef2e7388cce7544885dd7a06b985ca15a4
Vignesh Ezhil <86967110+viggy2000@users.noreply.github.com>
2022-04-06 00:17:46 +0800

    javadoc for enum BedType

--- hotel/src/main/java/com/hotel/system/enums/BedType.java
+++ hotel/src/main/java/com/hotel/system/enums/BedType.java
@@ -1,5 +1,36 @@
 package com.hotel.system.enums;
 
+/**
+ *  BedType that can be used
+ *  {@link #SINGLE}
+ *  {@link #DOUBLE}
+ *  {@link #SUPER_SINGLE}
+ *  {@link #QUEEN}
+ *  {@link #KING}
+ */
+
 public enum BedType {
-    SINGLE, DOUBLE, SUPER_SINGLE, QUEEN, KING 
+
+    /**
+     * SINGLE BED
+     */
+
+    SINGLE, 
+
+    /**
+     * DOUBLE BED
+     */
+    DOUBLE, 
+    /**
+     * SUPER_SINGLE BED
+     */
+    SUPER_SINGLE, 
+    /**
+     * QUEEN BED
+     */
+    QUEEN, 
+    /**
+     * KING BED
+     */
+    KING 
 }
