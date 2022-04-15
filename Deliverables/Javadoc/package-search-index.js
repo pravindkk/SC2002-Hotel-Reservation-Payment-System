cc6ad508a1a2e20a4b9fa76ea919fe5bc420e52c
Quek Kar Min <qkm2000@gmail.com>
2022-04-15 11:41:13 +0800

    Deliverables update
    - Created deliverables folder to sort and arrange deliverables
    - Created and updated javadoc

---
+++ Deliverables/Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.hotel.controller"},{"l":"com.hotel.db"},{"l":"com.hotel.system"},{"l":"com.hotel.system.enums"},{"l":"com.hotel.UI"}];updateSearchResults();
